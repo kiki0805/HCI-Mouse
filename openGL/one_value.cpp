4fd2ed4dc767ec0a0db29ed2643b2249a8f4f712
kiki0805 <kiki_hzz@163.com>
2018-11-14 17:02:16 +0800

    b/w test

--- openGL/one_value.cpp
+++ openGL/one_value.cpp
@@ -1,4 +1,5 @@
-#include <glad/glad.h>
+// #include <glad/glad.h>
+#include <GL/glxew.h>
 #include <GL/gl.h>
 #include <GL/glx.h>
 #include <GL/glu.h>
@@ -6,37 +7,13 @@
 #include <stdio.h>
 #include <assert.h>
 #include "utils.h"
+// g++ one_value.cpp -Ibuild/include build/src/glad.c -lGLEW -lglfw3 -lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -pthread -ldl
 
-// gcc one_value.c -lGLEW -lglfw3 -lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -pthread -ldl
-
-struct bit_ele* begin;
-
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "uniform vec4 OurColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, 800, 600);
-}
+// using namespace std;
 
 int main() {
-    char file_path[] = "share_data";
-    begin = read_swap_data(file_path);
     int init_ret = glfwInit();
+
     assert(init_ret == 1);
     
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -48,98 +25,26 @@ int main() {
         printf("Window or OpenGL context creation failed\n");
     }
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glViewport(0, 0, 800, 600);
     
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        printf("Failed to initialize GLAD\n");
-        return -1;
-    }
-
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s",infoLog);
-    }
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
-    }
-
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s", infoLog);
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-
-
-    float vertices[] = {
-        1.0f, 1.0f, 0.0f,   // 右上角
-        1.0f, -1.0f, 0.0f,  // 右下角
-        -1.0f, -1.0f, 0.0f, // 左下角
-        -1.0f, 1.0f, 0.0f   // 左上角
-    };
-
-    unsigned int indices[] = { // 注意索引从0开始! 
-        0, 1, 3, // 第一个三角形
-        1, 2, 3  // 第二个三角形
-    };
-
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
     bool flag = true;
     glfwSwapInterval(1);
+
     // wglSwapIntervalEXT(1);
     float timeValue = glfwGetTime();
     float current = timeValue;
     while (!glfwWindowShouldClose(window)) {
-        if(begin->bit == '0') 
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        else
-            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        if (begin->next == NULL) begin = (struct bit_ele*) begin->first_bit;
-        else begin = begin->next;
-        // if(flag) 
-        //     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        // else
+        // if(begin->bit == '0') 
         //     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        // flag = !flag;
+        // else
+        //     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // if (begin->next == NULL) begin = (struct bit_ele*) begin->first_bit;
+        // else begin = begin->next;
+        if(flag) 
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        else
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        flag = !flag;
 
         current = glfwGetTime();
         if(current-timeValue > (float)1.0/60.0 + 0.001) {
@@ -150,22 +55,11 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        // glUseProgram(shaderProgram);
-        // glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
-        // // if(flag)
-        // //     glUniform4f(vertexColorLocation, 0.0f, 0.0f, 0.0f, 1.0f);
-        // // else
-        // //     glUniform4f(vertexColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
         // Keep running
     }
 
     glfwTerminate();
-    free_bit_arr((struct bit_ele*)begin->first_bit);
     return 0;
 }
