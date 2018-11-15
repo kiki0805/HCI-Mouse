ac9155e0f7205dad815f4931df5def4b05521509
kiki0805 <kiki_hzz@163.com>
2018-11-15 19:14:33 +0800

    mesa swap control

--- openGL/one_value.cpp
+++ openGL/one_value.cpp
@@ -1,34 +1,48 @@
 // #include <glad/glad.h>
+#include <GL/glew.h>
 #include <GL/glxew.h>
-#include <GL/gl.h>
-#include <GL/glx.h>
-#include <GL/glu.h>
 #include <GLFW/glfw3.h>
+// #include "glxext.h"
+
 #include <stdio.h>
 #include <assert.h>
 #include "utils.h"
+#include <iostream>
 // g++ one_value.cpp -Ibuild/include build/src/glad.c -lGLEW -lglfw3 -lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -pthread -ldl
 
-// using namespace std;
+using namespace std;
 
 int main() {
     int init_ret = glfwInit();
-
     assert(init_ret == 1);
     
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
     GLFWwindow* window = glfwCreateWindow(800, 600, "My Title", NULL, NULL);
     if (!window) {
         printf("Window or OpenGL context creation failed\n");
     }
     glfwMakeContextCurrent(window);
     glViewport(0, 0, 800, 600);
-    
+
+    // if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    //     std::cout << "Failed to initialize OpenGL context" << std::endl;
+    //     return -1;
+    // }
+
+    GLenum err = glewInit();
+    assert(GLEW_OK == err);
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
+    if (glxewIsSupported("GLX_MESA_swap_control")) {
+        printf("OK, we can use GLX_MESA_swap_control\n");
+    } else {
+        printf("[WARNING] GLX_MESA_swap_control is NOT supported.\n");
+    }
+
+    glXSwapIntervalMESA(1);
     bool flag = true;
-    glfwSwapInterval(1);
+    // glXSwapIntervalSGI(1);
+    // cout<< rtn <<endl;
+    // if(rtn==GLX_BAD_CONTEXT || rtn == GLX_BAD_VALUE) return 0;
 
     // wglSwapIntervalEXT(1);
     float timeValue = glfwGetTime();
