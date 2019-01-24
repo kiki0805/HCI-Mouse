34a4e90861e4869a53de1a39c998e5246fdae4ae
kiki0805 <kiki_hzz@163.com>
2019-01-24 14:57:08 +0800

    angle detect

--- direction_judge.py
+++ direction_judge.py
@@ -1,12 +1,42 @@
 import math
 from scipy.spatial.distance import euclidean
 
-# red
-points1 =[((-694, -719), (694, 719)), ((-691, -722), (698, 715)), ((-723, 690), (739, -673)), ((-699, 714), (714, -699))]
 
-# white
-points2 = [((-706, -707), (707, 706)), ((-723, 690), (739, -673)), ((-731, 681), (754, -656)), ((-660, 751), (678, -735)), ((-699, 714), (714, -699)), ((-709, -704), (704, 709)), ((-694, 719), (719, -694))]
+def move_direction(red_points, white_points):
+    points1 = red_points
+    points2 = white_points
+    # red
+    # points1 =[((-694, -719), (694, 719)), ((-691, -722), (698, 715)), ((-723, 690), (739, -673)), ((-699, 714), (714, -699))]
 
+    # white
+    # points2 = [((-706, -707), (707, 706)), ((-723, 690), (739, -673)), ((-731, 681), (754, -656)), ((-660, 751), (678, -735)), ((-699, 714), (714, -699)), ((-709, -704), (704, 709)), ((-694, 719), (719, -694))]
+    dists = []
+    diff = {}
+    for p1 in points1:
+        for p2 in points2:
+            if p1[0][0] == p1[1][0] and p2[0][0] == p2[1][0]:
+                if p1[0][0] - p2[0][0] > 0:
+                    return 180
+                else:
+                    return 0
+            if p1 == p2 or p1[0][0] == p1[1][0] or p2[0][0] == p2[1][0]:
+                continue
+            line1 = calc_line(p1)
+            line2 = calc_line(p2)
+            # print('dist of k ' + str(abs(line1[0] - line2[0])))
+            if abs(line1[0] - line2[0]) < 0.1:
+                dist = calc_dist((line1, line2))
+                dists.append(dist)
+                diff[dist] = line1[2] - line2[2]
+
+    if dists == []:
+        return 'None'
+    if diff[min(dists)] < 0:
+        # print('↓')
+        return '↓'
+    else:
+        # print('↑')
+        return '↑'
 
 
 def calc_line(points):
@@ -28,28 +58,4 @@ def calc_dist(lines):
     return abs(c1 - c2) / (a*a + b*b)
 
 
-dists = []
-diff = {}
-for p1 in points1:
-    for p2 in points2:
-        if p1 == p2:
-            continue
-        line1 = calc_line(p1)
-        line2 = calc_line(p2)
-        if abs(line1[0] - line2[0]) < 0.1:
-            dist = calc_dist((line1, line2))
-            dists.append(dist)
-            diff[dist] = line1[2] - line2[2]
-        # print(calc_line(p1))
-        # print(calc_line(p2))
-        # dists.append(dist1)
 
-print(dists)
-print(min(dists))
-print(diff[min(dists)])
-if diff[min(dists)] < 0:
-    print('↓')
-else:
-    print('↑')
-# >0 downwards
-# <0 upwards
