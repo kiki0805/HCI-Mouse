76ccee7ee2fdefb64201b0fa4e057dedfd229251
kiki0805 <kiki_hzz@163.com>
2019-02-15 10:51:16 +0800

    angle check update

--- py_dev/angle.py
+++ py_dev/angle.py
@@ -141,6 +141,8 @@ def get_threshold(im, ratio=0.8):
 class AngleMeasurer:
     def __init__(self):
         self.img = Image.new("RGB", RESOLUTION)
+        self.red_img = None
+        self.white_img = None
         self.collect = 0
         self.cur = 'red'
         self.red_angle = None
@@ -148,8 +150,11 @@ class AngleMeasurer:
         self.r_ang = None
         self.w_ang = None
         self.raw_rx = None
+        self.raw_wx = None
         self.r_rhos = None
         self.w_rhos = None
+        self.r_votes = None
+        self.w_votes = None
         # self.white_angle = None
         # self.white_lines = None
     
@@ -191,17 +196,18 @@ class AngleMeasurer:
         self.collect = 3
         return TypeName.ANGLE_COLOR2RED, 0
 
-    def calc_angle(self, im):
-        mode = self.cur
+    def calc_angle(self, im_raw, mode, ref=None):
+        # mode = self.cur
         BORDER = 6
         if mode == 'white':
+            im = im_raw
             GAUSSIAN_STRENGTH = 1.5
             MIN_DISTANCE = 0
             MIN_ANGLE = 0
             NUM_PEAKS = 20
             TOLERATE_DIFF = 2
         elif mode == 'red':
-            im = ImageEnhance.Contrast(im).enhance(4)
+            im = ImageEnhance.Contrast(im_raw).enhance(4)
             GAUSSIAN_STRENGTH = 3
             MIN_DISTANCE = 2
             MIN_ANGLE = 1
@@ -246,27 +252,33 @@ class AngleMeasurer:
         theta = angles[index]
         ang = - x + 90
         # points = calc_points(rho, theta)
-        rtn = I_copy
+        # rtn = I_copy
         # rtn = draw_lines(I_copy, (((points[0], points[1]), (points[2], points[3])),))
 
-        ag = angles[index]
-        # if mode == 'red':
-        #     th = get_threshold(I_DCremove, 0.6)
-        #     BW_temp = I_DCremove < th
-        # BW[int(BORDER/2):19+BORDER-int(BORDER/2),int(BORDER/2):19+BORDER-int(BORDER/2)] = BW_temp
-        H, T, R = hough_line(BW * 255)
-        hspace, angles, dists = hough_line_peaks(H, T, R, min_distance=1, min_angle=1, num_peaks=NUM_PEAKS*50)
+        ag = theta
+        if ref is not None:
+            ag = ref
+        rows, cols = H.shape
         rhos = [[], []]
-        for i in range(len(angles)):
-            if math.degrees(abs(angles[i] - ag)) < TOLERATE_DIFF*2:
-                # print('same angle')
-                rhos[0].append(dists[i])
-            if abs(90 - math.degrees(abs(angles[i] - ag))) < TOLERATE_DIFF*2:
-                # print('verticle angle')
-                rhos[1].append(dists[i])
+        votes = [[], []]
+        for i in range(rows):
+            for j in range(cols):
+                if math.degrees(abs(T[j] - ag)) < 1 and H[i][j] > 4:
+                    # print('same angle')
+                    rhos[0].append(R[i])
+                    votes[0].append(H[i][j])
+                if abs(180 - math.degrees(abs(T[j] - ag))) < 1 and H[i][j] > 4:
+                    # print('same angle')
+                    rhos[0].append(R[i])
+                    votes[0].append(H[i][j])
+                if abs(90 - math.degrees(abs(T[j] - ag))) < 2 and H[i][j] > 4:
+                    # print('verticle angle')
+                    rhos[1].append(R[i])
+                    votes[1].append(H[i][j])
         # rhos[0]: rhos of the similar angle as returned one
         # rhos[1]: rhos of the verticle angle as returned one
-        return ang, rtn, rhos, x
+        return ang, rhos, votes, x, BW, H, rho
+
 
     def update(self, vals):
         count = 0
@@ -284,55 +296,125 @@ class AngleMeasurer:
         
         if self.cur == 'red':
             # return self.update_red_mode(threshold)
-            self.r_ang, _, self.r_rhos, self.raw_rx = self.calc_angle(self.img)
+            self.r_ang, _, self.r_rhos, self.r_votes, self.raw_rx, \
+                _, _, _ = self.calc_angle(self.img, 'red')
+            self.red_img = self.img.copy()
             return TypeName.ANGLE_COLOR2WHITE, 0
         else:
-            self.w_ang, _, self.w_rhos, _ = self.calc_angle(self.img)
+            self.w_ang, _, self.w_rhos, self.w_votes, self.raw_wx, \
+                _, _, _ = self.calc_angle(self.img, 'white')
             ang = self.get_angle()
+            self.white_img = self.img.copy()
             return TypeName.ANGLE, ang
             # return self.update_white_mode(threshold)
     
     def get_angle(self):
         r_ang=self.r_ang
         w_ang=self.w_ang
-        raw_rx=self.raw_rx
+        raw_wx = self.raw_wx
         r_rhos=self.r_rhos
         w_rhos=self.w_rhos
-        rhos = []
-        if abs(r_ang - w_ang) <= 45:
+        w_votes = self.w_votes
+        r_votes = self.r_votes
+        rhos = [] # 0: white 1: red
+        votes = [] # 0: white 1: red
+        if abs(r_ang - w_ang) <= 45 or abs(180 - abs(r_ang - w_ang)) <= 45:
             ang = w_ang
-            rhos.append(w_rhos[0])
-            rhos.append(r_rhos[0])
+            rhos = [w_rhos[0]]
+            votes = [w_votes[0]]
+            ref = math.radians(raw_wx)
         else:
             ang = w_ang + 90
-            rhos.append(w_rhos[1])
-            rhos.append(r_rhos[0])
-        min_dist = 999
-        min_dist_r = [[], []]
-        for r1 in rhos[0]:
-            for r2 in rhos[1]:
-                if r1 == r2:
-                    continue
-                if abs(r1-r2) < min_dist:
-                    min_dist = abs(r1-r2)
-                    min_dist_r[0] = [r1]
-                    min_dist_r[1] = [r2]
-                elif abs(r1-r2) == min_dist:
-                    if abs(np.mean(min_dist_r[1]) - r2) < min_dist:
-                        min_dist_r[1].append(r2)
-                        min_dist = abs(np.mean(min_dist_r[0]) - np.mean(min_dist_r[1]))
-            
-        min_dist_r = [np.mean(min_dist_r[0]), np.mean(min_dist_r[1])]
-        raw_rx = math.radians(raw_rx)
+            raw_wx = raw_wx + 90 if raw_wx < 0 else raw_wx - 90
+            rhos = [w_rhos[1]]
+            votes = [w_votes[1]]
+            ref = raw_wx # + 90 if raw_wx < 0 else raw_wx - 90
+            ref = math.radians(ref)
+        r_ang, r_rhos, r_votes, raw_rx, _, _, _ = self.calc_angle(self.red_img, 'red', ref=ref)
+        rhos = rhos + [r_rhos[0]]
+        votes = votes + [r_votes[0]]
 
-        positive = min_dist_r[0] - min_dist_r[1] > 0
+        assert rhos[0] != [] and rhos[1] != []
+
+        white_rho_blocks = []
+        tmp = []
+        for rho_i in range(len(rhos[0])):
+            r = rhos[0][rho_i]
+            v = votes[0][rho_i]
+            if v > 8:
+                if len(tmp) > 0:
+                    if abs(r -tmp[-1]) < 2:
+                        tmp.append(r)
+                    else:
+                        white_rho_blocks.append(tmp)
+                        tmp = []
+                else:
+                    tmp.append(r)
+            elif tmp != []:
+                white_rho_blocks.append(tmp)
+                tmp = []
+
+        red_rho_blocks = []
+        tmp = []
+        for rho_i in range(len(rhos[1])):
+            r = rhos[1][rho_i]
+            v = votes[1][rho_i]
+            if v > 8:
+                if len(tmp) > 0:
+                    if abs(r -tmp[-1]) < 2:
+                        tmp.append(r)
+                    else:
+                        if red_rho_blocks != []:
+                            if np.mean(tmp) != np.mean(red_rho_blocks[-1]):
+                                red_rho_blocks.append(tmp)
+                        else:
+                            red_rho_blocks.append(tmp)
+                        tmp = []
+                else:
+                    tmp.append(r)
+            elif tmp != []:
+                if red_rho_blocks != []:
+                    if np.mean(tmp) != np.mean(red_rho_blocks[-1]):
+                        red_rho_blocks.append(tmp)
+                else:
+                    red_rho_blocks.append(tmp)
+                tmp = []
+    
+        min_dists = []
+        for w_block in white_rho_blocks:
+            for r_block in red_rho_blocks:
+                mid_w = (max(w_block)+min(w_block)) / 2
+                mid_r = (max(r_block) + min(r_block)) / 2
+                if abs(mid_w - mid_r) < 3 and mid_w != mid_r:
+                    min_dists.append(mid_w - mid_r)
+        
+        if min_dists == []:
+            for w_block in white_rho_blocks:
+                for r_block in red_rho_blocks:
+                    mid_w = min(w_block)
+                    mid_r = max(r_block)
+                    if abs(mid_w - mid_r) < 3 and mid_w != mid_r:
+                        min_dists.append(mid_w - mid_r)
+
+        assert min_dists != []
+        positive_num, negative_num = 0, 0
+        for num in min_dists:
+            if num > 0:
+                positive_num += 1
+            else:
+                negative_num += 1
+        positive = np.mean(min_dists) > 0
         if ang > 180:
             ang -= 180
         ang -= 90
         if ang < 0:
             ang += 180
-        theta_positive = raw_rx > 0
-        if positive == theta_positive:
+        theta_positive = raw_wx > 0
+        if theta_positive and positive:
             return ang + 180
-        else:
+        elif theta_positive and not positive:
+            return ang
+        elif not theta_positive and not positive:
+            return ang + 180
+        elif not theta_positive and positive:
             return ang
