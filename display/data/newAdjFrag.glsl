aaeb362597f27b954134fd2b7f32cdd0becf128b
xudufy <xudufy@qq.com>
2019-03-16 19:40:14 +0800

    frt

--- display/data/newAdjFrag.glsl
+++ display/data/newAdjFrag.glsl
@@ -14,25 +14,28 @@ uniform int nbFrames;
 
 void main()
 {
+    vec4 rawColor = texture(rawScreen, (Position.xy + vec2(1.0, 1.0)) / vec2(2,2));
     rawColor = rawColor * 100.0f / 255.0f + vec4(80.0f/255.0f, 80.0f/255.0f,80.0f/255.0f,0);
     if (letThrough == 1) {
         FragColor = rawColor;
         FragColor.w = 1.0f;
     } else {
-        int x = (int) (TexCoord.x * 32);
-        int y = (int) (TexCoord.y * 32);
-        char coding[62];
+        int x, y;
+        y = int(TexCoord.x * 32);
+        x = int(TexCoord.y * 32);
+        int coding[10];
         coding[0] = 0;
-        char crc[62];
+        int crc[10];
         crc[0] = 0;
-        for (int i = 1; i < 50; i++) {
+        for (int i = 1; i < 8; i++) {
           coding[i] = ((x % i) & 1) ^ ((y % i) & 1);
           crc[i] = coding[i] ^ crc[i - 1];
         }
-        for (int i = 50; i < 62; i++) {
-          coding [i] = crc [i-50];
+        for (int i = 8; i < 10; i++) {
+          coding[i] = crc [i-10];
         }
         FragColor = rawColor - (coding[nbFrames % 62] & 1) * 80.0f;  
+        // FragColor = rawColor - 80.0f;
         FragColor.w = 1.0f;
     }
     //vec4(texture(texture0, TexCoord).xyz, texture(texture0, TexCoord).w) * (1 - letThrough);
