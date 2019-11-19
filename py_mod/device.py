a945d85810aa420d2026d137f1ef570993cd4eee
kiki0805 <kiki_hzz@163.com>
2019-11-19 10:42:54 +0800

    MIMO

--- py_mod/device.py
+++ py_mod/device.py
@@ -12,34 +12,38 @@ while True:
     print(mouse.sample())
 '''
 class Mouse:
-    def __init__(self, idVendor=0x046d, idProduct=0xc077, pixel_register=0x0D):
+    def __init__(self, idVendor=0x046d, idProduct=0xc077, pixel_register=0x0D, avg_register=0x0B):
         self.device = usb.core.find(idVendor=idVendor, idProduct=idProduct)
 
         if self.device.is_kernel_driver_active(0):
             self.device.detach_kernel_driver(0)
 
         self.device.set_configuration()
-        self.register = pixel_register
+        self.pixel_register = pixel_register
+        self.avg_register = avg_register
     
     def write_register(self):
         self.device.ctrl_transfer(
             bmRequestType = 0x40, #Write
             bRequest = 0x01,
             wValue = 0x0000,
-            wIndex = self.register, #PIX_GRAB register value
+            wIndex = self.pixel_register, #PIX_GRAB register value
             data_or_wLength = None
         )
     
-    def read_register(self):
+    def read_register(self, register):
         response = self.device.ctrl_transfer(bmRequestType = 0xC0, #Read
             bRequest = 0x01,
             wValue = 0x0000,
-            wIndex = self.register, #PIX_GRAB register value
+            wIndex = register,
             data_or_wLength = 1
         )
         return response
     
     def sample(self):
         self.write_register()
-        point = self.read_register()
-        return time.time(), point
+        pixel = self.read_register(self.pixel_register)
+        pixel_sample = (time.time(), pixel)
+        avg = self.read_register(self.avg_register)
+        avg_sample = (time.time(), avg)
+        return pixel_sample, avg_sample
